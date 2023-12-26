28ad85b549f13c6fb6c4f825111ecf6f49f26edc
AhmedKhattab24 <154933305+AhmedKhattab24@users.noreply.github.com>
2023-12-27 01:12:26 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=1;
+    do
+    {
+        printf("the number using do..while loop: %i\n",i);
+        i++;
+    }
+    while(i<=10);
+
+    return 0;
+}
